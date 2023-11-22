1e278376d199db58e27c982e2426dbd1c3aa60da
Tyler Schlichenmeyer <tyler.schlichenmeyer@gmail.com>
2023-11-22 07:55:34 -0600

    checkpoint

--- vns/generate_profile_report.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from ydata_profiling import ProfileReport
-
-trials = pd.read_parquet("data/trials+latency.parquet.gzip")[
-    ["outcome_latency", "target_amplitude"]
-]
-
-
-ProfileReport(trials).to_file("src/vns/assets/profile_report.html")

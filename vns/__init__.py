6dd604aa36e8a9d67858f8d38d3438dd6eb9031f
Ty Schlichenmeyer <ty.schlich@gmail.com>
2023-12-11 05:31:17 +0000

    devcontainer build

--- vns/__init__.py
+++ vns/__init__.py
@@ -1,29 +1,10 @@
 __version__ = "0.0.1"
 
 from datatree import DataTree
+import numpy as np
 import xarray as xr
 
-import vaex as vx
 
-
-def mat_to_vx(mat):
-    """Convert a numpy array from scipy.io.loadmat to a vaex DataFrame.
-
-    Parameters
-    ----------
-    mat : numpy.ndarray
-        A numpy array from scipy.io.loadmat.
-
-    Returns
-    -------
-    vaex.dataframe.DataFrame
-        A vaex DataFrame.
-
-    """
-    return vx.from_arrays(time_focused=mat["PDS"].item()[0])
-
-
-
-def extinction_learning(datatree: DataTree) -> xr.DataSet:
+def extinction_learning(datatree: DataTree) -> xr.Dataset:
     """Produce extinction learning results."""
-    return xr.DataSet()
+    return xr.DataArray(data=np.ndarray(shape=(3)), dims=("Trial"))

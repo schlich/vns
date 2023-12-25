a4cea656882c331104d25c6906ccdff65681b7f3
Ty Schlichenmeyer <ty.schlich@gmail.com>
2023-12-25 06:34:24 +0000

    rearrange notebooks

--- vns/__init__.py
+++ vns/__init__.py
@@ -1,10 +1,46 @@
 __version__ = "0.0.1"
 
-from datatree import DataTree
-import numpy as np
-import xarray as xr
+import datetime
+import os
+
+import pandas as pd
+import scipy
+
+# from datatree import DataTree
+# import numpy as np
+# import xarray as xr
 
 
-def extinction_learning(datatree: DataTree) -> xr.Dataset:
-    """Produce extinction learning results."""
-    return xr.DataArray(data=np.ndarray(shape=(3)), dims=("Trial"))
+# def extinction_learning(datatree: DataTree) -> xr.Dataset:
+#     """Produce extinction learning results."""
+#     return xr.DataArray(data=np.ndarray(shape=(3)), dims=("Trial"))
+
+
+def date_from_filename(filename):
+    return datetime.datetime.strptime(
+        filename[12:-4],
+        "%d_%m_%Y_%H_%M",
+    )
+
+def session_attrs(filename):
+    c = scipy.io.loadmat(
+        "data/BFINAC_VNS/" + filename,
+        squeeze_me=True,
+    )["c"]
+    return {
+        field_name: data
+        for field_name, data in {
+            field_name: c.item()[i] for i, field_name in enumerate(c.dtype.names)
+        }.items()
+        if not isinstance(data, np.ndarray)
+    }
+
+
+def sessions():
+    pd.DataFrame.from_records(
+    [session_attrs(filename) for filename in os.listdir("data/BFINAC_VNS")],
+    index=pd.Index(
+        [date_from_filename(filename) for filename in os.listdir("data/BFINAC_VNS")],
+        name="datetime",
+    ),
+)

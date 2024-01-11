c2e3af56cdf0baa9b2d5fc6a39e19bbe08e58f4a
Ty Schlichenmeyer <ty.schlich@gmail.com>
2024-01-11 09:38:17 +0000

    rm quarto

--- main.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    use plotly::{Plot, Scatter};
-
-    let mut plot = Plot::new();
-    let trace = Scatter::new(vec![0, 1, 2], vec![2, 1, 0]);
-    plot.add_trace(trace);
-
-    plot.write_html("out.html");
-}

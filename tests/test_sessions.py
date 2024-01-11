a4556dea2e2af596a64e32fe657c38a3fe3d54c7
Ty Schlichenmeyer <ty.schlich@gmail.com>
2024-01-11 18:39:13 +0000

    hatch tricks

--- tests/test_sessions.py
+++ tests/test_sessions.py
@@ -1,11 +1,12 @@
 from pathlib import Path
 
 import pandas as pd
-
 from vns import Session
 
 
 def test_session_start_time(tmpdir: Path):
-    file_path = tmpdir / "BFnovelinac" / "mat" / Path("BFnovelinac_01_02_2019_15_03.mat")
+    file_path = (
+        tmpdir / "BFnovelinac" / "mat" / Path("BFnovelinac_01_02_2019_15_03.mat")
+    )
     session = Session(file_path)
     assert session.start_time == pd.Timestamp("2019-02-01 15:03:00")
